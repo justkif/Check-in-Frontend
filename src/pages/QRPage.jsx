732a5898097dd5cdd58bc68c60ee6f61cdb4e1f1
kyky <kyle.vuanh@gmail.com>
2025-04-15 21:36:55 +0700

    [CREATE] QRPage

--- src/pages/QRPage.jsx
+++ src/pages/QRPage.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from 'react';
+import generateQR from '../apis/generateQR';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+
 export default function QRPage() {
-    return 'QRPage';
+    const [src, setSrc] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    useEffect(() => {
+        const handleGenerateQR = async () => {
+            try {
+                setLoading(true);
+                setSrc(await generateQR());
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        handleGenerateQR();
+    }, []);
+    return (
+        <div>
+            {src && <img src={src} className='w-full'/>}
+            {error && <Error error={error}/>}
+            {loading && <Loading />}
+        </div>
+    );
 }
