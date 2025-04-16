6ab3b758be185abc10a37039f4fb8d3cfb6cc20a
kyky <kyle.vuanh@gmail.com>
2025-04-16 21:37:34 +0700

    [CREATE] ManagerPage

--- src/pages/ManagerPage.jsx
+++ src/pages/ManagerPage.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from 'react';
+import exportExcel from '../apis/exportExcel'
+import runnerGetAll from '../apis/runnerGetAll';
+import Table from '../components/Table';
+import Loading from '../components/Loading';
+
 export default function ManagerPage() {
-    return 'ManagerPage';
+    const [runners, setRunners] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const getAll = async () => {
+            try {
+                setLoading(true);
+                setRunners(await runnerGetAll());
+            } catch (err) {
+            } finally {
+                setLoading(false);
+            }
+        }
+        getAll();
+    }, []);
+    return (
+        <div>
+            <button className='px-4 py-2 rounded bgColor text-white font-semibold my-4' onClick={exportExcel}>Export Excel</button>
+            <Table type={'manager'} data={runners}/>
+            {loading && <Loading />}
+        </div>
+    );
 }
